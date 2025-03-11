7c0ed6f45ad1c176c6593a182ad1d3de2556848a
Natt Nguyen <natt.lefevre@gmail.com>
2025-03-11 14:57:15 -0700

    banner

--- src/app/(frontend)/blog/[slug]/page.tsx
+++ src/app/(frontend)/blog/[slug]/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 import { getPayload } from "payload"
 import configPromise from "@payload-config"
 import RichText from "@/components/rich-text-renderer"
-import styles from "@/app/(frontend)/blog/[slug]/page.module.css"
 import { draftMode } from "next/headers"
 import { LivePreviewListener } from "@/app/(frontend)/blog/[slug]/live-preview-listener"
+import "@/app/(frontend)/blog/[slug]/page.css"
 
 export interface BlogPostPageProps {
   params: Promise<{
@@ -39,10 +39,10 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
   if (!post) return notFound()
 
   return (
-    <div className="mt-64 flex flex-col px-20">
+    <div className="mt-64 flex flex-col">
       {draft && <LivePreviewListener />}
 
-      <div className="mx-auto mt-48 mb-32 flex w-full max-w-[63.6rem] flex-col">
+      <div className="mx-auto mt-48 mb-36 flex w-full max-w-[63.6rem] flex-col px-20">
         <Link
           className="text-14 mb-16 w-fit text-[#8e8c8f] transition-colors hover:text-[#111013]"
           href="/blog"
@@ -52,33 +52,47 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
 
         <h1 className="font-400 text-36/125 mb-24">{post.title}</h1>
 
-        {post.author &&
-          typeof post.author === "object" &&
-          "name" in post.author && (
-            <p>
-              By {post.author.name}{" "}
-              <span className="text-[#8e8c8f]">· {post.author.title}</span>
-            </p>
-          )}
+        <div className="flex gap-x-8">
+          <div className="size-48 bg-gray-100" />
+          <div className="flex flex-col items-start">
+            {post.author &&
+              typeof post.author === "object" &&
+              "name" in post.author && (
+                <p>
+                  By {post.author.name}{" "}
+                  <span className="text-[#8e8c8f]">· {post.author.title}</span>
+                </p>
+              )}
 
-        {post["date-published"] && (
-          <p className="text-14 text-[#8e8c8f]">
-            {new Date(post["date-published"]).toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-          </p>
-        )}
+            {post["date-published"] && (
+              <p className="text-14 text-[#8e8c8f]">
+                {new Date(post["date-published"]).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}{" "}
+                · 0 min read
+              </p>
+            )}
+          </div>
+        </div>
       </div>
 
+      <div className="aspect-16-9 mx-auto mb-36 flex h-full w-full max-w-[96rem] flex-col">
+        <div className="h-full w-full bg-gray-100" />
+      </div>
+
+      {/* Content */}
       {post.content && (
-        <div className={styles.content}>
-          <RichText data={post.content} />
+        <div className="mx-auto flex w-full max-w-[63.6rem] flex-col px-20">
+          <div className="content">
+            <RichText data={post.content} />
+          </div>
         </div>
       )}
 
-      <div className="mx-auto flex w-full max-w-[63.6rem] flex-col">
+      {/* Share */}
+      <div className="mx-auto flex w-full max-w-[63.6rem] flex-col px-20">
         <div className="mb-128 border-t-2 border-solid border-gray-300 pt-24">
           <p className="text-24">Share</p>
         </div>
