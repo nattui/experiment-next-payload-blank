6c37dadd9247d1b08c13c69503f3b8bb57d1274f
Natt Nguyen <natt.lefevre@gmail.com>
2025-03-11 20:07:09 -0700

    rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ import { withPayload } from "@payloadcms/next/withPayload"
 
 const nextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        destination: "https://www.typeface.ai/blog/:path*",
+        source: "/old-blog/:path*",
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
